0d3f9cc08863ed63bdb694afa6a8166b735c1463
codes-akshat <mrinalinisinha718@gmail.com>
2026-08-30 01:26:05 +0530

    project finished

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", () => {
+            console.log("MongoDB connected");
+        });
+
+        await mongoose.connect(process.env.MONGODB_URI);
+    } catch (error) {
+        console.error("MongoDB connection failed:");
+        console.error(error.message);
+    }
+};
+
+export default connectDB;
